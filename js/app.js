3d667f1a0ef4cc4071da22ed013a66ed2aaf356e
yoni1994 <yoniklein1994@gmail.com>
2021-07-02 10:42:17 -0500

    organize code

--- js/app.js
+++ js/app.js
@@ -65,10 +65,6 @@ continueBtn.addEventListener('click', handleContinue)
 
 /*-------------------------------- Functions --------------------------------*/
 
-//reset game
-function reset() {
-    document.location.reload(true)
-}
 
 //set up easy game
 function handleEasy() {
@@ -181,6 +177,23 @@ function createBoard() {
     init()
 }
 
+function placeMines() {
+    while (mineCount < minesToPlace) {
+        let rngRow = Math.floor(Math.random() * boardHeight) + 1;
+        let rngColumn = Math.floor(Math.random() * boardWidth) + 1;
+        for (let i = 0; i < cells.length; i++) {
+            row = cells[i].getAttribute('data-row')
+            column = cells[i].getAttribute('data-column')
+            isMined = cells[i].getAttribute('data-mine')
+            if(row == rngRow && column == rngColumn && isMined == 0) {
+                cells[i].setAttribute('data-mine', 1)
+                // cells[i].innerText = '💣' /*---for testing purposes to see the mines*/
+                mineCount++
+                break
+            }
+        }
+    }
+}
 
 board.addEventListener('click', handleClick)
 board.addEventListener('contextmenu', handleRightClick)
@@ -232,29 +245,52 @@ function handleRightClick(evt) {
 }
 
 //places minesToPlace # of mines in random cells
-function placeMines() {
-    while (mineCount < minesToPlace) {
-        let rngRow = Math.floor(Math.random() * boardHeight) + 1;
-        let rngColumn = Math.floor(Math.random() * boardWidth) + 1;
-        for (let i = 0; i < cells.length; i++) {
-            row = cells[i].getAttribute('data-row')
-            column = cells[i].getAttribute('data-column')
-            isMined = cells[i].getAttribute('data-mine')
-            if(row == rngRow && column == rngColumn && isMined == 0) {
-                cells[i].setAttribute('data-mine', 1)
-                // cells[i].innerText = '💣' /*---for testing purposes to see the mines*/
-                mineCount++
-                break
-            }
-        }
-    }
-}
 
 //set number to be revealed when cell is clicked
 function giveCellsNeighborValues() {
     for (let i = 0; i < cells.length; i++) {
         cells[i].setAttribute('data-nearby-mine-cells', countMines(cells[i]))
     }
+}
+
+//get the neighboring cells
+function getNeighbors(cell) {
+    let neighborCells = []
+    let cellRow = parseInt(cell.getAttribute('data-row'))
+    let cellColumn = parseInt(cell.getAttribute('data-column'))
+    if (cellRow !== 1) {
+        if (cellColumn != 1) {
+            const topLeftCell = document.querySelector(`[data-row='${cellRow-1}'][data-column='${cellColumn-1}']`)
+            neighborCells.push(topLeftCell)
+        }
+        const topMiddleCell = document.querySelector(`[data-row='${cellRow-1}'][data-column='${cellColumn}']`)
+        neighborCells.push(topMiddleCell)
+        if (cellColumn != boardWidth) {
+            const topRightCell = document.querySelector(`[data-row='${cellRow-1}'][data-column='${cellColumn+1}']`)
+            neighborCells.push(topRightCell)
+        }
+    }
+    if (cellColumn != 1) {
+        const leftCell = document.querySelector(`[data-row='${cellRow}'][data-column='${cellColumn-1}']`)
+        neighborCells.push(leftCell)
+    }
+    if (cellColumn != boardWidth) {
+        const rightCell = document.querySelector(`[data-row='${cellRow}'][data-column='${cellColumn+1}']`)
+        neighborCells.push(rightCell)
+    }
+    if (cellRow != boardHeight) {
+        if (cellColumn != 1) {
+            const bottomLeftCell = document.querySelector(`[data-row='${cellRow+1}'][data-column='${cellColumn-1}']`)
+            neighborCells.push(bottomLeftCell)
+        }
+        const bottomMiddleCell = document.querySelector(`[data-row='${cellRow+1}'][data-column='${cellColumn}']`)
+        neighborCells.push(bottomMiddleCell)
+        if (cellColumn != boardWidth) {
+            const bottomRightCell = document.querySelector(`[data-row='${cellRow+1}'][data-column='${cellColumn+1}']`)
+            neighborCells.push(bottomRightCell)
+        }
+    }
+    return neighborCells
 }
 
 
@@ -332,50 +368,11 @@ function bubble(cell) {
                 bubbleSquares = bubbleSquares.concat(bubble(neighbor))
             }
         }
-
+        
     })
     return bubbleSquares
 }
 
-//get the neighboring cells
-function getNeighbors(cell) {
-    let neighborCells = []
-    let cellRow = parseInt(cell.getAttribute('data-row'))
-    let cellColumn = parseInt(cell.getAttribute('data-column'))
-    if (cellRow !== 1) {
-        if (cellColumn != 1) {
-            const topLeftCell = document.querySelector(`[data-row='${cellRow-1}'][data-column='${cellColumn-1}']`)
-            neighborCells.push(topLeftCell)
-        }
-        const topMiddleCell = document.querySelector(`[data-row='${cellRow-1}'][data-column='${cellColumn}']`)
-        neighborCells.push(topMiddleCell)
-        if (cellColumn != boardWidth) {
-            const topRightCell = document.querySelector(`[data-row='${cellRow-1}'][data-column='${cellColumn+1}']`)
-            neighborCells.push(topRightCell)
-        }
-    }
-    if (cellColumn != 1) {
-        const leftCell = document.querySelector(`[data-row='${cellRow}'][data-column='${cellColumn-1}']`)
-        neighborCells.push(leftCell)
-    }
-    if (cellColumn != boardWidth) {
-        const rightCell = document.querySelector(`[data-row='${cellRow}'][data-column='${cellColumn+1}']`)
-        neighborCells.push(rightCell)
-    }
-    if (cellRow != boardHeight) {
-        if (cellColumn != 1) {
-            const bottomLeftCell = document.querySelector(`[data-row='${cellRow+1}'][data-column='${cellColumn-1}']`)
-            neighborCells.push(bottomLeftCell)
-        }
-        const bottomMiddleCell = document.querySelector(`[data-row='${cellRow+1}'][data-column='${cellColumn}']`)
-        neighborCells.push(bottomMiddleCell)
-        if (cellColumn != boardWidth) {
-            const bottomRightCell = document.querySelector(`[data-row='${cellRow+1}'][data-column='${cellColumn+1}']`)
-            neighborCells.push(bottomRightCell)
-        }
-    }
-    return neighborCells
-}
 
 //render a game over
 function gameOver() {
@@ -430,4 +427,9 @@ function handleContinue() {
         colorScheme.dark ? document.body.style.background = "url('../images/night-explosion.jpeg')" : document.body.style.background = "url('../images/day-explosion.png')"
         document.body.style.backgroundSize = 'cover'
     }
+}
+
+//reset game
+function reset() {
+    document.location.reload(true)
 }
